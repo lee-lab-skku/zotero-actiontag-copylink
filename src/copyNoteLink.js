1cb98baaa77cd53d42fff857f360d182704c2fa1
Shin Taeha <mirinae3145@g.skku.edu>
2025-10-14 17:14:51 +0900

    fix: plain text of note link

--- src/copyNoteLink.js
+++ src/copyNoteLink.js
@@ -2,9 +2,11 @@ if (item.isNote() && Zotero.BetterNotes) {
     uri = Zotero.BetterNotes.api.convert.note2link(item);
 }
 
+const text = `${item.getField('title')} (${Zotero.Items.getTopLevel([item])[0].getField("citationKey")})`;
+
 const clipboard = new Zotero.ActionsTags.api.utils.ClipboardHelper();
 clipboard.addText(uri, "text/unicode");
-clipboard.addText(`<a href="${uri}">test text</a>`, "text/html");
+clipboard.addText(`<a href="${uri}">${text}</a>`, "text/html");
 clipboard.copy();
 
 return "Copied Better Notes link to clipboard.";
