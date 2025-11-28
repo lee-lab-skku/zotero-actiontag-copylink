175fb157de3e3122d12bbd4321a7a914bd33afc8
Shin Taeha <mirinae3145@g.skku.edu>
2025-11-28 16:53:57 +0900

    fetch latest actions

--- src/updateActions.js
+++ src/updateActions.js
@@ -1,4 +1,11 @@
 const SOURCE_URL = 'https://cdn.skypack.dev/-/js-yaml@v4.1.1-8B0j8wiUmEXyI4j5ClPv/dist=es2019,mode=imports/optimized/js-yaml.js';
+const TARGET_URL = 'https://api.github.com/repos/lee-lab-skku/zotero-actionstags-actions/releases/latest';
+const TARGET_FILE_NAME = 'zotero-actionstags-backup.yml';
+
+const j = await fetch(TARGET_URL).then(res => res.json());
+const assetUrl = j.assets.find(asset => asset.name === TARGET_FILE_NAME).browser_download_url;
+const actions = await fetch(assetUrl).then(res => res.text());
+
 const code = await fetch(SOURCE_URL).then(res => res.text());
 code=code.replace(/export\s+default\s+[^;]+;|export\s*\{[^}]*\};?/g, "");
 
