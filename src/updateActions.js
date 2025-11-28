d65c5a88e888feef36ed11fb499498792c90e701
Shin Taeha <mirinae3145@g.skku.edu>
2025-11-28 17:32:31 +0900

    refactor

--- src/updateActions.js
+++ src/updateActions.js
@@ -2,14 +2,15 @@ const SOURCE_URL = 'https://cdn.skypack.dev/-/js-yaml@v4.1.1-8B0j8wiUmEXyI4j5ClP
 const TARGET_URL = 'https://api.github.com/repos/lee-lab-skku/zotero-actionstags-actions/releases/latest';
 const TARGET_FILE_NAME = 'zotero-actionstags-backup.yml';
 
-const j = await fetch(TARGET_URL).then(res => res.json());
-const assetUrl = j.assets.find(asset => asset.name === TARGET_FILE_NAME).browser_download_url;
+const latest = await fetch(TARGET_URL).then(res => res.json());
+const latestVersion = latest.tag_name;
+const assetUrl = latest.assets.find(asset => asset.name === TARGET_FILE_NAME).browser_download_url;
 let actions = await fetch(assetUrl).then(res => res.text());
 
 let code = await fetch(SOURCE_URL).then(res => res.text());
-code=code.replace(/export\s+default\s+[^;]+;|export\s*\{[^}]*\};?/g, "");
+code = code.replace(/export\s+default\s+[^;]+;|export\s*\{[^}]*\};?/g, "");
 
-sb = Cu.Sandbox(Services.scriptSecurityManager.getSystemPrincipal());
+const sb = Cu.Sandbox(Services.scriptSecurityManager.getSystemPrincipal());
 Cu.evalInSandbox(code, sb);
 
 actions = sb.load(actions).actions;
