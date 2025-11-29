10e7875b2902d116681a885e14ceed9e1fc2366e
Shin Taeha <mirinae3145@g.skku.edu>
2025-11-29 18:50:47 +0900

    fix idiots

--- src/reviewNote.js
+++ src/reviewNote.js
@@ -2,16 +2,17 @@ const PREF_GROUP_KEY = 'extensions.zotero-actionstags.group-id';
 const PREF_COLLECTION_KEY = 'extensions.zotero-actionstags.review-collection-key';
 
 (async () => {
+    const targetItem = item;
+
     if (Zotero.ActionsTags.__reviewNoteRunning) return;
     Zotero.ActionsTags.__reviewNoteRunning = true;
 
-    let selected;
+    const selected = new Object();
     let ok;
 
-    const groupID = Zotero.Prefs.get(PREF_GROUP_KEY);
+    let groupID = Zotero.Prefs.get(PREF_GROUP_KEY);
     if (!groupID) {
         const groups = Zotero.Groups.getAll();
-        selected = new Object();
         ok = await Services.prompt.select(null, 'Organization', 'Please select your organization.', groups.map(g => g.name), selected);
         if (!ok)
             return 1;
@@ -19,11 +20,10 @@ const PREF_COLLECTION_KEY = 'extensions.zotero-actionstags.review-collection-key
         Zotero.Prefs.set(PREF_GROUP_KEY, groupID);
     }
 
-    const collectionKey = Zotero.Prefs.get(PREF_COLLECTION_KEY);
+    let collectionKey = Zotero.Prefs.get(PREF_COLLECTION_KEY);
     if (!collectionKey) {
         const targetLibraryID = Zotero.Groups.getLibraryIDFromGroupID(groupID);
         const cols = Zotero.Collections.getByLibrary(targetLibraryID);
-        selected = new Object();
         ok = await Services.prompt.select(null, 'Collection', 'Please select the collection to review notes in.', cols.map(c => c.name), selected);
         if (!ok)
             return 1;
@@ -31,7 +31,7 @@ const PREF_COLLECTION_KEY = 'extensions.zotero-actionstags.review-collection-key
         Zotero.Prefs.set(PREF_COLLECTION_KEY, collectionKey);
     }
 
-    if (!item.getCollections().map(c => Zotero.Collections.get(c).name).includes(collectionKey))
+    if (!targetItem.getCollections().map(c => Zotero.Collections.get(c).name).includes(collectionKey))
         return 0;
 
     Zotero.ActionsTags.__reviewNoteRunning = false;
