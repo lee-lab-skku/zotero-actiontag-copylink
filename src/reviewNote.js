b2d2129666a58882f48fc8b33de333312414f826
Shin Taeha <mirinae3145@g.skku.edu>
2025-11-29 20:05:38 +0900

    attach note

--- src/reviewNote.js
+++ src/reviewNote.js
@@ -1,5 +1,6 @@
 const PREF_GROUP_KEY = 'extensions.zotero-actionstags.group-id';
 const PREF_COLLECTION_KEY = 'extensions.zotero-actionstags.review-collection-key';
+const PREF_NAME = 'extensions.zotero-actionstags.reviewer-name';
 
 (async () => {
     const targetItem = item;
@@ -14,8 +15,10 @@ const PREF_COLLECTION_KEY = 'extensions.zotero-actionstags.review-collection-key
     if (!groupID) {
         const groups = Zotero.Groups.getAll();
         ok = await Services.prompt.select(null, 'Organization', 'Please select your organization.', groups.map(g => g.name), selected);
-        if (!ok)
+        if (!ok) {
+            Zotero.ActionsTags.__reviewNoteRunning = false;
             return 1;
+        }
         groupID = groups[selected.value].id;
         Zotero.Prefs.set(PREF_GROUP_KEY, groupID);
     }
@@ -31,8 +34,44 @@ const PREF_COLLECTION_KEY = 'extensions.zotero-actionstags.review-collection-key
         Zotero.Prefs.set(PREF_COLLECTION_KEY, collectionKey);
     }
 
-    if (!targetItem.getCollections().map(c => Zotero.Collections.get(c).name).includes(collectionKey))
+    if (!targetItem.getCollections().map(c => Zotero.Collections.get(c).key).includes(collectionKey)) {
+        Zotero.ActionsTags.__reviewNoteRunning = false;
         return 0;
+    }
+
+    let reviewerName = Zotero.Prefs.get(PREF_NAME);
+    if (!reviewerName) {
+        ok = await Services.prompt.prompt(null, 'Reviewer Name', 'Please enter your name to be added to the review note. (In Korean, no space.)', selected, null, {});
+        if (!ok) {
+            Zotero.ActionsTags.__reviewNoteRunning = false;
+            return 1;
+        }
+        reviewerName = selected.value.trim();
+        Zotero.Prefs.set(PREF_NAME, reviewerName);
+    }
+
+    const now = new Date().getTime();
+    let cnt = 0;
+    const dates = [];
+
+    while (cnt <= 7) {
+        dates.push(new Date(now + cnt * 86400000).toISOString());
+        cnt++;
+    }
+
+    ok = await Services.prompt.select(null, 'Review Date', 'Select the review date.', dates.map(d => d.slice(5, 10)), selected);
+    if (!ok) {
+        Zotero.ActionsTags.__reviewNoteRunning = false;
+        return 1;
+    }
+
+    const td = dates[selected.value];
+    const noteContent = `<h1>${td.slice(2,4)}${td.slice(5,7)}${td.slice(8,10)} ${reviewerName}</h1>`;
+
+    const note = new Zotero.Item('note');
+    note.parentID = targetItem.id;
+    note.setNote(noteContent);
+    await note.saveTx();
 
     Zotero.ActionsTags.__reviewNoteRunning = false;
     return 0;
